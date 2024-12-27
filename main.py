55c8d7ebcafa8ff007d315c0f3eb2a55af46a2d3
andy <andy.kdry98@gmail.com>
2024-12-27 16:52:02 +0100

    changed word into line

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ date_now = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 def textToSpeach(text):
     tts = gTTS(text, lang="en")
     tts.save(date_now+"-"+text+".mp3")
-    return 
+
+with open("text.txt", encoding="utf8") as f:
+    for line in f:
+        #for word in line.split():
+        try:
+            textToSpeach(line)
+            print(line)
+        except Exception:
+            pass
